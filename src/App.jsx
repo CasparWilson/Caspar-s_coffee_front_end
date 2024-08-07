4a0ca51cd9326456df6de01133797b457ceaecb7
Caspar <casparwilson111@gmail.com>
2024-08-07 15:55:04 +0100

    initial layout set out

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
-
 //@ts-ignore ignore typescript issues with import.meta
-const apiBaseURL = import.meta.env.VITE_API_BASE_URL;
+import axios from "axios";
+import React from "react";
 
 function App() {
+    const apiBaseURL = import.meta.env.BASE_URL;
+    if (!apiBaseURL) {
+        throw new Error("missing import.meta.env.BASE_URL");
+    }
+    console.log(apiBaseURL);
+
+    const [displayedCustomer, setDisplayedCustomer] = React.useState(null);
+    const [inputFields, setInputFields] = React.useState({
+        searchByID: "",
+        addCustomer: "",
+    });
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setInputFields((prevTextFieldValues) => ({
+            ...prevTextFieldValues,
+            [name]: value,
+        }));
+    }
+
+    const handleSearch = async () => {
+        const searchURL =
+            apiBaseURL + `/customerLoyalty/${inputFields.searchByID}`;
+        const newSearchResult = await axios.get(searchURL);
+        console.log(newSearchResult.data);
+        setDisplayedCustomer(newSearchResult.data);
+        setInputFields((prevInputFields) => ({
+            ...prevInputFields,
+            searchByID: "",
+        }));
+    };
+
     return (
         <div>
-            <h1>App component</h1>
-            <DemonstratingProps />
-            <p>
-                The value of the environment variable VITE_API_BASE_URL is:
-                <code>{apiBaseURL}</code>
-            </p>
+            <h1>Coffee App</h1>
+
+            <h4>Please enter the customer ID</h4>
+            <input
+                name="searchByID"
+                onChange={handleInputChange}
+                value={inputFields.searchByID}
+            ></input>
+            <button onClick={handleSearch}>Search</button>
         </div>
     );
 }
